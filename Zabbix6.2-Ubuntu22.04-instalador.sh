33cef5c7a750fa129c42a8ba7d6b7c3159e3abc8
AlefMartins <63752383+AlefMartins@users.noreply.github.com>
2023-01-06 23:16:54 -0300

    Update Zabbix6.2-Ubuntu22.04-instalador.sh

--- Zabbix6.2-Ubuntu22.04-instalador.sh
+++ Zabbix6.2-Ubuntu22.04-instalador.sh
@@ -73,7 +73,7 @@ php8.1-fpm.service 1>>${log} 2>&1
 printf "[${GREEN}INFO${RESET}] Configuracao do MySQL\n" 2>&1 | tee --append ${log}
 mkdir -p /var/lib/zabbix && chown zabbix:zabbix /var/lib/zabbix 1>>${log} 2>&1
 printf "[client]\nuser=zbx_monitor\npassword='${mysql_zbx_monitor_passwd}'\nhost=${mysql_server_address}\n" >/var/lib/zabbix/.my.cnf
-#printf "[client]\nuser=root\npassword='${mysql_root_passwd}'\nhost=${mysql_server_address}\n" >/root/.my.cnf
+printf "[client]\nuser=root\npassword='${mysql_root_passwd}'\nhost=${mysql_server_address}\n" >/root/.my.cnf
 
 printf "[${GREEN}INFO${RESET}] Criando banco de dados e usuarios\n" 2>&1 | tee --append ${log}
 echo "CREATE DATABASE zabbix CHARACTER SET utf8 COLLATE utf8_bin;" >/tmp/zabbix_mysql.sql
